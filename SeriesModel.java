1d3a0ee663b4bc8a36589c78784de31167883a45
st10528245 <st10528245@rcconnect.edu.za>
2026-08-24 18:29:35 +0200

    Add files via upload

--- SeriesModel.java
+++ SeriesModel.java
@@ -1,68 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.mycompany.seriesmodel;
 
 /**
  *
  * @author Student
  */
-public class SeriesModel {
+class SeriesModel {
 
-    private String SeriesId;
-    private String SeriesName;
-    private String SeriesAge;
-    private String SeriesNumberOfEpisodes;
 
-    public SeriesModel(String SeriesId, String SeriesName,
-                       String SeriesAge, String SeriesNumberOfEpisodes) {
-
-        this.SeriesId = SeriesId;
-        this.SeriesName = SeriesName;
-        this.SeriesAge = SeriesAge;
-        this.SeriesNumberOfEpisodes = SeriesNumberOfEpisodes;
+    SeriesModel(String string, String bargain_Hunters, String string0, String string1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
-    public String getSeriesId() {
-        return SeriesId;
-    }
-
-    public void setSeriesId(String SeriesId) {
-        this.SeriesId = SeriesId;
-    }
-
-    public String getSeriesName() {
-        return SeriesName;
-    }
-
-    public void setSeriesName(String SeriesName) {
-        this.SeriesName = SeriesName;
-    }
-
-    public String getSeriesAge() {
-        return SeriesAge;
-    }
-
-    public void setSeriesAge(String SeriesAge) {
-        this.SeriesAge = SeriesAge;
-    }
-
-    public String getSeriesNumberOfEpisodes() {
-        return SeriesNumberOfEpisodes;
-    }
-
-    public void setSeriesNumberOfEpisodes(String SeriesNumberOfEpisodes) {
-        this.SeriesNumberOfEpisodes = SeriesNumberOfEpisodes;
-    }
-
-    @Override
-    public String toString() {
-
-        return "SERIES ID: " + SeriesId
-                + "\nSERIES NAME: " + SeriesName
-                + "\nSERIES AGE RESTRICTION: " + SeriesAge
-                + "\nSERIES NUMBER OF EPISODES: "
-                + SeriesNumberOfEpisodes;
-    }
+    
 }
